c9c01711fc66330281d565c58e054aa69404b1c0
Anjula Perera <anjulaperera98@gmail.com>
2023-02-15 10:27:46 +0530

    extracted table component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import TableHeader from "./reusable/tableHeader";
-import TableBody from "./reusable/tableBody";
+import Table from "./reusable/table";
 import Like from "./reusable/like";
 
 class MoviesTable extends Component {
@@ -32,14 +31,12 @@ class MoviesTable extends Component {
     const { movies, onSort, sortColumn } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
